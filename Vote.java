6f0ae778c18d41508fa98c464b7a2eeb6bf1930a
Pradeepkumar1811 <149380561+Pradeepkumar1811@users.noreply.github.com>
2023-10-30 17:46:39 +0530

    Create Vote.java

---
+++ Vote.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Main{
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    int age=sc.nextInt();
+    if(age<18){
+        System.out.println("You are not eligible");
+    }
+    else{
+        System.out.println("You are eligible");
+    } 
+  }
+}
