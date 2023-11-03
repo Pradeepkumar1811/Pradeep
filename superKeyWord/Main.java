2ac9757196ae7433fed039725d97698378d9c1fb
Pradeepkumar1811 <149380561+Pradeepkumar1811@users.noreply.github.com>
2023-11-03 11:58:42 +0530

    Update Main.java

--- superKeyWord/Main.java
+++ superKeyWord/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		Student s = new Student();
-        s.display();
-        s.msg();
+        	s.display();
+        	s.msg();
 	}
 
 }
